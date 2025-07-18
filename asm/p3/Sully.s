4c3ebf21dfe7ab9c729ce8b77ca039ddbb0b5c27
jun han <junhan@balaenaquant.com>
2025-07-18 21:32:44 +0800

    feat: fixes

--- asm/p3/Sully.s
+++ asm/p3/Sully.s
@@ -15,9 +15,18 @@ mov [y], rax
 push out_name_fmt
 mov rdi, out_name
 mov rsi, out_name_fmt
-mov rdx, [y]
+mov rdx, [x]
 call sprintf
 pop rax
+push exec_cmd_fmt
+mov rdi, comd
+mov rsi, exec_cmd_fmt
+mov rdx, [x]
+call sprintf
+pop rax
+mov eax, [x]
+cmp eax, 0
+jl end
 mov rax, 2
 mov rdi, out_name
 mov rsi, 1 | 0o100
@@ -38,15 +47,6 @@ pop rax
 mov rax, 2
 mov rdi, [fd]
 syscall
-push exec_cmd_fmt
-mov rdi, comd
-mov rsi, exec_cmd_fmt
-mov rdx, [y]
-call sprintf
-pop rax
-mov eax, [x]
-cmp eax, 1
-jle end
 push comd
 mov rdi, comd
 call system
@@ -57,6 +57,6 @@ xor rdi, rdi
 syscall  
 section .data
 x dd 5
-exec_cmd_fmt: db 'nasm -felf64 Sully_%1$d.s -o Sully_%1$d.o && gcc -no-pie Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d',0
+exec_cmd_fmt: db 'nasm -felf64 Sully_%1$d.s -o Sully_%1$d.o && gcc -no-pie Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d && rm -f *.o',0
 out_name_fmt: db 'Sully_%d.s',0
-out_content_fmt: db "global main%1$cextern sprintf%1$cextern dprintf%1$cextern system%1$csection .bss%1$ccomd resb 100%1$cout_name resb 10%1$cfd resb 8%1$cy resb 8%1$csection .text%1$cmain:%1$cmov rax, [x]%1$csub rax, 1%1$cmov [y], rax%1$cpush out_name_fmt%1$cmov rdi, out_name%1$cmov rsi, out_name_fmt%1$cmov rdx, [y]%1$ccall sprintf%1$cpop rax%1$cmov rax, 2%1$cmov rdi, out_name%1$cmov rsi, 1 | 0o100%1$cmov rdx, 0o644%1$csyscall%1$cmov [fd], rax%1$cpush out_content_fmt%1$csub rsp, 8%1$cpush 34%1$cmov rdi, [fd]%1$cmov rsi, out_content_fmt%1$cmov rdx, 10%1$cmov rcx, [y]%1$cmov r8, out_content_fmt%1$cmov r9, 37%1$ccall dprintf%1$cpop rax%1$cmov rax, 2%1$cmov rdi, [fd]%1$csyscall%1$cpush exec_cmd_fmt%1$cmov rdi, comd%1$cmov rsi, exec_cmd_fmt%1$cmov rdx, [y]%1$ccall sprintf%1$cpop rax%1$cmov eax, [x]%1$ccmp eax, 1%1$cjle end%1$cpush comd%1$cmov rdi, comd%1$ccall system%1$cpop rax%1$cend:%1$cmov rax, 60%1$cxor rdi, rdi%1$csyscall  %1$csection .data%1$cx dd %2$d%1$cexec_cmd_fmt: db 'nasm -felf64 Sully_%4$c1$d.s -o Sully_%4$c1$d.o && gcc -no-pie Sully_%4$c1$d.o -o Sully_%4$c1$d && ./Sully_%4$c1$d',0%1$cout_name_fmt: db 'Sully_%4$cd.s',0%1$cout_content_fmt: db %5$c%3$s%5$c,0",0
+out_content_fmt: db "global main%1$cextern sprintf%1$cextern dprintf%1$cextern system%1$csection .bss%1$ccomd resb 100%1$cout_name resb 10%1$cfd resb 8%1$cy resb 8%1$csection .text%1$cmain:%1$cmov rax, [x]%1$csub rax, 1%1$cmov [y], rax%1$cpush out_name_fmt%1$cmov rdi, out_name%1$cmov rsi, out_name_fmt%1$cmov rdx, [x]%1$ccall sprintf%1$cpop rax%1$cpush exec_cmd_fmt%1$cmov rdi, comd%1$cmov rsi, exec_cmd_fmt%1$cmov rdx, [x]%1$ccall sprintf%1$cpop rax%1$cmov eax, [x]%1$ccmp eax, 0%1$cjl end%1$cmov rax, 2%1$cmov rdi, out_name%1$cmov rsi, 1 | 0o100%1$cmov rdx, 0o644%1$csyscall%1$cmov [fd], rax%1$cpush out_content_fmt%1$csub rsp, 8%1$cpush 34%1$cmov rdi, [fd]%1$cmov rsi, out_content_fmt%1$cmov rdx, 10%1$cmov rcx, [y]%1$cmov r8, out_content_fmt%1$cmov r9, 37%1$ccall dprintf%1$cpop rax%1$cmov rax, 2%1$cmov rdi, [fd]%1$csyscall%1$cpush comd%1$cmov rdi, comd%1$ccall system%1$cpop rax%1$cend:%1$cmov rax, 60%1$cxor rdi, rdi%1$csyscall  %1$csection .data%1$cx dd %2$d%1$cexec_cmd_fmt: db 'nasm -felf64 Sully_%4$c1$d.s -o Sully_%4$c1$d.o && gcc -no-pie Sully_%4$c1$d.o -o Sully_%4$c1$d && ./Sully_%4$c1$d && rm -f *.o',0%1$cout_name_fmt: db 'Sully_%4$cd.s',0%1$cout_content_fmt: db %5$c%3$s%5$c,0",0
--- c/p3/Sully.c
+++ c/p3/Sully.c
@@ -3,22 +3,21 @@
 int main()
 {
 	int x = 5;
-	int y = x - 1;
 
 	char out_name[10];
-	sprintf(out_name,"Sully_%d.c", y);
-	FILE *fptr = fopen(out_name, "w");
-	char *out_contents = "#include <stdio.h>%1$c#include <stdlib.h>%1$cint main()%1$c{%1$c	int x = %3$d;%1$c	int y = x - 1;%1$c%1$c	char out_name[10];%1$c	sprintf(out_name,%2$cSully_%5$cd.c%2$c, y);%1$c	FILE *fptr = fopen(out_name, %2$cw%2$c);%1$c	char *out_contents = %2$c%4$s%2$c;%1$c	fprintf(fptr, out_contents, 10, 34, y, out_contents, 37);%1$c	fclose(fptr);%1$c%1$c	char compile_cmd[100];%1$c	sprintf(compile_cmd,%2$cgcc Sully_%5$c1$d.c -o Sully_%5$c1$d%2$c, y);%1$c%1$c	char exec_cmd[100];%1$c	sprintf(exec_cmd,%2$c./Sully_%5$cd%2$c, y);%1$c	if (x > 1)%1$c	{%1$c		system(compile_cmd);%1$c		system(exec_cmd);%1$c	}%1$c}";
-	fprintf(fptr, out_contents, 10, 34, y, out_contents, 37);
-	fclose(fptr);
+	sprintf(out_name,"Sully_%d.c", x);
+	char *out_contents = "#include <stdio.h>%1$c#include <stdlib.h>%1$cint main()%1$c{%1$c	int x = %3$d;%1$c%1$c	char out_name[10];%1$c	sprintf(out_name,%2$cSully_%5$cd.c%2$c, x);%1$c	char *out_contents = %2$c%4$s%2$c;%1$c%1$c	char compile_cmd[100];%1$c	sprintf(compile_cmd,%2$cgcc Sully_%5$c1$d.c -o Sully_%5$c1$d%2$c, x);%1$c%1$c	char exec_cmd[100];%1$c	sprintf(exec_cmd,%2$c./Sully_%5$cd%2$c, x);%1$c	if (x >= 0)%1$c	{%1$c		FILE *fptr = fopen(out_name, %2$cw%2$c);%1$c		fprintf(fptr, out_contents, 10, 34, x - 1, out_contents, 37);%1$c		fclose(fptr);%1$c		system(compile_cmd);%1$c		system(exec_cmd);%1$c	}%1$c}";
 
 	char compile_cmd[100];
-	sprintf(compile_cmd,"gcc Sully_%1$d.c -o Sully_%1$d", y);
+	sprintf(compile_cmd,"gcc Sully_%1$d.c -o Sully_%1$d", x);
 
 	char exec_cmd[100];
-	sprintf(exec_cmd,"./Sully_%d", y);
-	if (x > 1)
+	sprintf(exec_cmd,"./Sully_%d", x);
+	if (x >= 0)
 	{
+		FILE *fptr = fopen(out_name, "w");
+		fprintf(fptr, out_contents, 10, 34, x - 1, out_contents, 37);
+		fclose(fptr);
 		system(compile_cmd);
 		system(exec_cmd);
 	}
